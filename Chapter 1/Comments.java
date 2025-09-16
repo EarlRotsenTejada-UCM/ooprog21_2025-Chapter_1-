7313078171ca7b79c6a711eec2046989f04d5106
EarlRotsenTejada-UCM <djrotsentejada@gmail.com>
2025-09-16 08:57:45 +0800

    Add files via upload

---
+++ Chapter 1/Comments.java
@@ -0,0 +1,16 @@
+public class Comments {
+    // This is a single-line comment
+
+    /*
+     This is a multi-line comment.
+     It can span multiple lines.
+    */
+
+    /**
+     * This is a documentation comment.
+     * Used to generate JavaDoc.
+     */
+    public static void main(String[] args) {
+        System.out.println("Comments example");
+    }
+}
---
+++ Chapter 1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Please be careful with my heart");
+        System.out.println("I don't want to fall apart");
+        System.out.println("Love me tender, love me sweet");
+        System.out.println("Say that you love me too");
+    }
+}
